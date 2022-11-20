45b8915ecf4b99d2da30d25d8cb67bdd2201aa22
Karteek <kosana.kartheek@gmail.com>
2022-11-20 17:09:04 +0530

    ThreadLocal

--- src/test/java/com/Assignment/TestCases/TestCases.java
+++ src/test/java/com/Assignment/TestCases/TestCases.java
@@ -12,13 +12,10 @@ public class TestCases extends BaseClass {
 	@Test
 	public void Test1() {
 
-		
+		System.out.println("Test1 Method Thread ID: " + Thread.currentThread().getId());
 		homePage.checkIfElementIsDisplayed(homePage.emailElement);
-//		test.log(Status.PASS, homePage.emailElement.getAttribute("type") + " element is present");
 		homePage.checkIfElementIsDisplayed(homePage.passwordElement);
-//		test.log(Status.PASS, homePage.passwordElement.getAttribute("type") + " element is present");
 		homePage.checkIfElementIsDisplayed(homePage.signInElement);
-//		test.log(Status.PASS, homePage.signInElement.getText() + " element is present");
 		homePage.emailElement.sendKeys("karteek@gmail.com");
 		homePage.passwordElement.sendKeys("******");
 
@@ -27,56 +24,51 @@ public class TestCases extends BaseClass {
 	@Test
 	public void Test2() {
 
-		
+		System.out.println("Test2 Method Thread ID: " + Thread.currentThread().getId());
+
 		homePage.checkValuesInListGroup();
-//		test.log(Status.PASS, "There are three values in the group");
 		homePage.checkSecondListItem();
-//		test.log(Status.PASS, "Second list item's value is set to \"List Item 2\"");
 		homePage.checkSecondListItemBadgeValue();
-//		test.log(Status.PASS, "Second list item's badge value is 6");
 
 	}
 
 	@Test
 	public void Test3() throws InterruptedException {
 
-		
+		System.out.println("Test3 Method Thread ID: " + Thread.currentThread().getId());
 		homePage.ScrolltotheElement(homePage.dropDownOption);
 		homePage.checkDefaultSelectedValue();
-//		test.log(Status.PASS, "\"Option 1\" is the default selected value");
 		homePage.selectOption3();
 	}
 
 	@Test
 	public void Test4() {
 
-		
+		System.out.println("Test4 Method Thread ID: " + Thread.currentThread().getId());
 		homePage.ScrolltotheElement(homePage.enabledButtonElement);
 		homePage.checkIfFirstButtonisEnabled();
-//		test.log(Status.PASS, "First button is Enabled");
 		homePage.checkIfButtonisDisabled(homePage.disabledButtonElement);
-//		test.log(Status.PASS, "Second button is Disabled");
+
 	}
 
 	@Test
 	public void Test5() {
 
-	
+		System.out.println("Test5 Method Thread ID: " + Thread.currentThread().getId());
 		homePage.ScrolltotheElement(homePage.test5Div);
 		homePage.ExplicitWait(homePage.test5Button);
 		homePage.clickOnButton();
-//		test.log(Status.PASS, "Success message is displayed");
 		homePage.checkIfButtonisDisabled(homePage.test5Button);
-//		test.log(Status.PASS, "Button is now disabled");
+
 	}
 
 	@Test
 	public void Test6() throws IOException {
 
-		
+		System.out.println("Test6 Method Thread ID: " + Thread.currentThread().getId());
 		homePage.ScrolltotheElement(homePage.test6Div);
 		String cellValue = homePage.findValueOfCell(driver, 2, 2);
 		System.out.println(cellValue);
-//		test.log(Status.PASS, "Value of the cell is " + cellValue);
+
 	}
 }
